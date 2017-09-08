edeaa98c932876a4937aee6fd5e8bc4f5fa39551
Shinsuke Sugaya <shinsuke@apache.org>
2017-09-08 09:09:38 +0900

    use apache github repository

--- tests/before_script.travis.sh
+++ tests/before_script.travis.sh
@@ -5,8 +5,7 @@ cd predictionio-setup
 curl -o bin/pio-setup https://raw.githubusercontent.com/jpioug/predictionio-setup/master/bin/pio-setup
 chmod +x bin/pio-setup
 
-#PIO_GIT_USER=apache PIO_GIT_BRANCH=develop PIO_EVENTDATA_REFRESH=true ./bin/pio-setup deploy
-PIO_EVENTDATA_REFRESH=true ./bin/pio-setup deploy
+PIO_GIT_USER=apache PIO_GIT_BRANCH=develop PIO_EVENTDATA_REFRESH=true ./bin/pio-setup deploy
 touch target/pio-setup.log
 tail -f target/pio-setup.log &
 ./bin/pio-setup start
