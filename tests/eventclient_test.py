e5cd6200be129c736835cb23a0211d5264afc171
Shinsuke Sugaya <shinsuke@apache.org>
2017-09-09 23:37:03 +0900

    add tests

--- tests/eventclient_test.py
+++ tests/eventclient_test.py
@@ -8,6 +8,7 @@ from predictionio import NotFoundError
 
 app_name ='EventClientApp'
 access_key = 'EVENT_CLIENT_TEST'
+channel = 'Test'
 
 class EventClientTest(unittest.TestCase):
 
@@ -171,6 +172,80 @@ class EventClientTest(unittest.TestCase):
         response = client.record_user_action_on_item("buy", "foo", "bar", action_properties)
         print(response)
         self.assertEqual(response.status, 201)
+
+
+    def test_eventclient_channel(self):
+        subprocess.call(['pio', 'app', 'channel-new', app_name, channel])
+
+        client = EventClient(access_key=access_key, url="http://127.0.0.1:7070",
+                             channel=channel)
+
+        # Check status
+        print("Check status")
+        print(client.get_status())
+        self.assertEqual(client.get_status(), {'status': 'alive'})
+
+        # First event
+        first_event_properties = {
+            "prop1" : 1,
+            "prop2" : "value2",
+            "prop3" : [1, 2, 3],
+            "prop4" : True,
+            "prop5" : ["a", "b", "c"],
+            "prop6" : 4.56 ,
+            }
+        first_event_time = datetime(
+            2004, 12, 13, 21, 39, 45, 618000, pytz.timezone('US/Mountain'))
+        first_event_response = client.create_event(
+            event="my_event",
+            entity_type="user",
+            entity_id="uid",
+            properties=first_event_properties,
+            event_time=first_event_time,
+            )
+        print("First Event response")
+        print(first_event_response)
+        self.assertEqual(first_event_response.status, 201)
+
+        # Second event
+        second_event_properties = {
+            "someProperty" : "value1",
+            "anotherProperty" : "value2",
+            }
+        second_event_response = client.create_event(
+            event="my_event",
+            entity_type="user",
+            entity_id="uid",
+            target_entity_type="item",
+            target_entity_id="iid",
+            properties=second_event_properties,
+            event_time=datetime(2014, 12, 13, 21, 38, 45, 618000, pytz.utc))
+        print("Second Event response")
+        print(second_event_response)
+        self.assertEqual(second_event_response.status, 201)
+
+
+        # Get the first event from Event Server
+        first_event_id = first_event_response.json_body["eventId"]
+        print("Get Event")
+        event = client.get_event(first_event_id)
+        print(event)
+        self.assertEqual(event.get('eventId'), first_event_id)
+
+        # Delete the first event from Event Server
+        print("Delete Event")
+        delete_response = client.delete_event(first_event_id)
+        print(delete_response)
+        self.assertEqual(delete_response.decode('utf-8'), '{"message":"Found"}')
+
+        # Delete the first event from Event Server again should yield exception.
+        print("Delete Event Again")
+        try:
+            delete_response = client.delete_event(first_event_id)
+            self.fail()
+        except NotFoundError as ex:
+            print("The expected error: {0}".format(ex))
+
 
 if __name__ == "__main__":
     unittest.main()
